509087525d4b6d66dbdb7ce575c70fdd9ef58599
Lovesh Karadia <loveshkaradia@gmail.com>
2026-05-26 17:52:19 +0530

    Update login message from branch B

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Learning Git");
-        System.out.println("Nothing to do here");
+        System.out.println("Login API from branch B");
     }
 }
